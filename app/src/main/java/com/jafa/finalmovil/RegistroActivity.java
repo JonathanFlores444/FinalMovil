0b31bf6287ff3962f1b81b033e966abfa74e4b55
JonathanFlores444 <jonathan.flores289@ist17dejulio.edu.ec>
2024-02-12 04:15:19 -0500

    diseño login activity

--- app/src/main/java/com/jafa/finalmovil/RegistroActivity.java
+++ app/src/main/java/com/jafa/finalmovil/RegistroActivity.java
@@ -2,21 +2,13 @@ package com.jafa.finalmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    
+public class RegistroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_registro);
     }
-
-
 }
