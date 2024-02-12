8b625fd993fce0c74b34c9e587a97a807cf44493
JonathanFlores444 <jonathan.flores289@ist17dejulio.edu.ec>
2024-02-12 16:15:13 -0500

    activity resultado

--- app/src/main/java/com/jafa/finalmovil/MainActivity.java
+++ app/src/main/java/com/jafa/finalmovil/MainActivity.java
@@ -2,20 +2,30 @@ package com.jafa.finalmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    TextView tvIrRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        tvIrRegistro = findViewById(R.id.lblRegistrarse);
+        tvIrRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent IrRegistro = new Intent(MainActivity.this, RegistroActivity.class);
+                startActivity(IrRegistro);
+            }
+        });
     }
 
 
--- app/src/main/java/com/jafa/finalmovil/RegistroActivity.java
+++ app/src/main/java/com/jafa/finalmovil/RegistroActivity.java
@@ -2,13 +2,25 @@ package com.jafa.finalmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegistroActivity extends AppCompatActivity {
-
+    TextView tvIrLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        tvIrLogin = findViewById(R.id.txtRegAtras);
+        tvIrLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IrLogin = new Intent(RegistroActivity.this, MainActivity.class);
+                startActivity(IrLogin);
+                finish();
+            }
+        });
     }
 }
