056a0b44cfda31b6dd1fd9fd96e92ee41ed1b19c
JonathanFlores444 <jonathan.flores289@ist17dejulio.edu.ec>
2024-02-17 23:25:42 -0500

    validaciones y arreglo de logica del login

--- app/src/main/java/com/jafa/finalmovil/MainActivity.java
+++ app/src/main/java/com/jafa/finalmovil/MainActivity.java
@@ -47,30 +47,34 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String user = edusuario.getText().toString();
                 String pass = edcontrasenia.getText().toString();
-                if(!user.isEmpty() || !pass.isEmpty()){
+                if(!user.isEmpty() && !pass.isEmpty()){
                     loginWS();
                 }else {
-                    Toast.makeText(MainActivity.this, "Escriba bien no sea gil", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "No sea gil, no deje vacíos", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
     private void loginWS(){
-        String url = "http://192.168.1.60/WS/webapi.php?op=login&user='"+edusuario.getText().toString()+"'&pass='"+edcontrasenia.getText().toString()+"'";
+        String url = "http://192.168.1.60/WS/webapi.php?op=login&user="+edusuario.getText().toString()+"&pass="+edcontrasenia.getText().toString()+"";
         StringRequest postRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    int userType = jsonObject.getInt("user_type");
-                    Toast.makeText(MainActivity.this,userType,Toast.LENGTH_SHORT).show();
-                    if("0".equals(userType)){
-                        Toast.makeText(MainActivity.this, "Estudiambre",Toast.LENGTH_SHORT).show();
-                    } else if ("1".equals(userType)){
-                        Toast.makeText(MainActivity.this, "Dochente",Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(MainActivity.this, "Datos Incorrectos",Toast.LENGTH_SHORT).show();
-                    }
+                    JSONArray jsonResponse = new JSONArray(response);
+
+                        String userType = jsonResponse.getString(0);
+                        if ("0".equals(userType)) {
+                            Intent IrInicio = new Intent(MainActivity.this, InicioActivity.class);
+                            startActivity(IrInicio);
+                        } else if ("1".equals(userType)) {
+                            Intent IrInicio = new Intent(MainActivity.this, InicioActivity.class);
+                            startActivity(IrInicio);
+                        } else if("2".equals(userType)) {
+                            Toast.makeText(MainActivity.this, "Datos Incorrectos", Toast.LENGTH_SHORT).show();
+                        }
+
+
 
                 } catch (JSONException e) {
                     e.printStackTrace();
