f0edcd50e469cdf8c0ce57620a210d304a2c6cac
JonathanFlores444 <jonathan.flores289@ist17dejulio.edu.ec>
2024-02-18 19:22:38 -0500

    validaciones en recuperacion de cuenta y conexion al web service

--- app/src/main/java/com/jafa/finalmovil/RecuperarActivity.java
+++ app/src/main/java/com/jafa/finalmovil/RecuperarActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RecuperarActivity extends AppCompatActivity {
     EditText edCedula, edUsuario, edNuevaCon, edConfirmaCon;
@@ -36,21 +49,67 @@ public class RecuperarActivity extends AppCompatActivity {
         btActualiza.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String cedula = edCedula.getText().toString();
-                String usuario = edUsuario.getText().toString();
-                String newcontra = edNuevaCon.getText().toString();
-                String confcontra = edConfirmaCon.getText().toString();
+                String cedular = edCedula.getText().toString();
+                String usuarior = edUsuario.getText().toString();
+                String newcontrar = edNuevaCon.getText().toString();
+                String confcontrar = edConfirmaCon.getText().toString();
 
-                if(cedula.isEmpty() && usuario.isEmpty() && newcontra.isEmpty() && confcontra.isEmpty()){
-                    Toast.makeText(RecuperarActivity.this, "No deje campos vacios", Toast.LENGTH_SHORT).show();
-                }else if(!newcontra.equals(confcontra)){
-                    Toast.makeText(RecuperarActivity.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
-                    edNuevaCon.setText("");
-                    edConfirmaCon.setText("");
-                }else{
-                    //Aqui se supone que va el metodo que actualiza
+                if (!cedular.isEmpty() && !usuarior.isEmpty() && !newcontrar.isEmpty() && !confcontrar.isEmpty()) {
+                    if (newcontrar.equals(confcontrar)) {
+                        newpassWS(cedular, usuarior, confcontrar);
+                    } else {
+                        Toast.makeText(RecuperarActivity.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                        edNuevaCon.setText("");
+                        edConfirmaCon.setText("");
+                    }
+                } else {
+                    Toast.makeText(RecuperarActivity.this, "No deje campos vacíos", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+    private void newpassWS(String ced, String usu, String con) {
+        String url = "http://192.168.1.60/WS/webapi.php?op=newpass";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonResponse = new JSONArray(response);
+                    String resultado = jsonResponse.getString(0);
+
+                    if ("3".equals(resultado)) {
+                        Intent IrLogin = new Intent(RecuperarActivity.this, MainActivity.class);
+                        startActivity(IrLogin);
+                        finish();
+                        Toast.makeText(RecuperarActivity.this, "Se actualizo correctamente", Toast.LENGTH_SHORT).show();
+                    } else if("0".equals(resultado)){
+                        Toast.makeText(RecuperarActivity.this, "Usuario no encontrado", Toast.LENGTH_SHORT).show();
+                        edCedula.setText("");
+                        edUsuario.setText("");
+                        edNuevaCon.setText("");
+                        edConfirmaCon.setText("");
+
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("error", error.getMessage());
+            }
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("cedula",ced);
+                params.put("usuario", usu);
+                params.put("contrasenia", con);
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(postRequest);
     }
 }
